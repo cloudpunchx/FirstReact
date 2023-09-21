e3dfc5c035271afb181a0de3738794fa9a59d2b5
Siobhan Bonardi <siobhanbonardi@hotmail.com>
2023-09-21 17:53:54 -0600

    Added Button.tsx component. Added to App, tested and working as expected.

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+interface Props {
+  children: string;
+  //   the ? makes it optional, and how we have it set allows only these exact matches to be allowed
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button type="button" className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
