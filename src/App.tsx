0f045ec6e1b665907f7498f351fb21ee9f4c7d6a
Siobhan Bonardi <siobhanbonardi@hotmail.com>
2023-09-21 18:50:42 -0600

    Button click to show Alert, added close button to Alert to close.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-      <Button color="secondary" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
         Click Me!
       </Button>
     </div>
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,13 +3,21 @@ import { ReactNode } from "react";
 // Using the children prop, we can pass children to the component
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
 // This is destructuring the props
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, onClose }: Props) => {
   return (
-    <div className="alert alert-danger" role="alert">
+    <div className="alert alert-danger alert-dismissible" role="alert">
       {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
     </div>
   );
 };
